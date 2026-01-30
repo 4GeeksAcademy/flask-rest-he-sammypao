bf249ef8fd8284ed5c6e89a66947f20215d2cb5e
Shammy Murillo <75450484+sammypao@users.noreply.github.com>
2026-01-30 16:50:10 +0000

    primera parte terminada

--- src/models.py
+++ src/models.py
@@ -1,19 +1,90 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, Boolean, ForeignKey, Enum
+import enum
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(250), nullable=False)
+    firstname: Mapped[str] = mapped_column(String(250), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(250), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Follower(db.Model):
+    __tablename__ = 'followers'
+
+    user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
+
+    def serialize(self):
+        return{
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id
+        }
+    
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    User_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "User_id": self.User_id
+        }
+
+
+class MediaTypeEnum(enum.Enum):
+    image = "image"
+    video = "video"
+
+class Media(db.Model):
+    __tablename__ = "media"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[MediaTypeEnum] = mapped_column(Enum(MediaTypeEnum))
+    Url: Mapped[str] = mapped_column(String(250))
+    Post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type.value if self.type else None,
+            "Url": self.Url,
+            "Post_id": self.Post_id
+        }
+    
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    Comment_text: Mapped[str] = mapped_column(String(250))
+    autor_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    Post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "Comment_text": self.Comment_text,
+            "autor_id": self.autor_id,
+            "Post_id": self.Post_id
+        }
+    
+    
